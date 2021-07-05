69b976e6d06bd00f30e0b269de9af2032e5d87ea
crytome1995 <ethan.lebioda@gmail.com>
2021-07-05 15:34:49 -0500

    f

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+url="https://${GIT_USERNAME}:${GIT_TOKEN}@github.com/crytome1995/Charts.git"
+git remote add origin ${url}
 git clone --branch $1 https://github.com/crytome1995/Charts.git
 cd Charts/clicker
 ls -la
 sed -i "/^\([[:space:]]*tag: \).*/s//\1$2/" values.yaml
 cat values.yaml
-url="https://${GIT_USERNAME}:${GIT_TOKEN}@github.com/crytome1995/Charts.git"
 git commit values.yaml -m "Releasing tag dev $2"
 git push ${url} $1
